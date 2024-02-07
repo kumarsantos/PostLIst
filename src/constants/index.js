5bc3ecc6c2dccc4883a5933021edd446bd887ad0
Santosh Kumar Sah <Ssah5803@gmail.com>
2024-02-07 11:44:10 +0530

    Route implemented with HOME and Favourite component and static data created

---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+export const listOfPosts = [
+  {
+    id: 1,
+    title: "First Post",
+    description: "This is the description of the first post.",
+    author: "John Doe",
+    date: "2024-02-05",
+    image:
+      "https://plus.unsplash.com/premium_photo-1664006988924-16f386bcd40e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+    description: "This is the description of the second post.",
+    author: "Jane Smith",
+    date: "2024-02-06",
+    image:
+      "https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "Third Post",
+    description: "This is the description of the third post.",
+    author: "Alice Johnson",
+    date: "2024-02-07",
+    image:
+      "https://plus.unsplash.com/premium_photo-1679064065167-1c7133791c99?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGJvb2t8ZW58MHx8MHx8fDA%3D",
+  },
+];
